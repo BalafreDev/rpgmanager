ab9b767bc17e9bfd217da7cc8f74223942a249f3
Balafre <pierre-giraud@live.fr>
2024-04-17 04:52:01 +0200

    patch connection to DB error

--- src/api/users/route.ts
+++ src/api/users/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import dbConnect from "@/lib/dbConnect"
+// import dbConnect from "@/lib/dbConnect"
 import {User} from "@/models/user";
 import { NextResponse } from "next/server";
 
@@ -16,24 +16,24 @@ import { NextResponse } from "next/server";
 
 // }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await dbConnect()
-  if (req.method === 'GET') {
-    try {
-      const users = await User.find({}); /* find all the data in our database */
-      return NextResponse.json({ success: true, data: users });
-    } catch (error) {
-      return NextResponse.json({ success: false, error });
-    }
-  }
-  else {
-    // await dbConnect()
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   await dbConnect()
+//   if (req.method === 'GET') {
+//     try {
+//       const users = await User.find({}); /* find all the data in our database */
+//       return NextResponse.json({ success: true, data: users });
+//     } catch (error) {
+//       return NextResponse.json({ success: false, error });
+//     }
+//   }
+//   else {
+//     // await dbConnect()
 
-    try {
+//     try {
 
-    } catch (error) {
+//     } catch (error) {
 
-    }
+//     }
 
-  }
-}
+//   }
+// }
